f73016466484d4b079c1771318105b4cdaea2d93
Joel Wanner <joel.wanner@inf.ethz.ch>
2021-03-31 17:32:16 +0200

    Generate customer configurations

--- src/gen.py
+++ src/gen.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python3
 import json
+import os
 
+GEN_DIR = 'gen'
 HOST_KEY = 'public-ip'
 USER_KEY = 'ssh-user'
-DEFAULT_USER = 'scionlab'
 
-remotes = {}
+nodes = {}
+customers = {}
 
 with open('nodes.json', 'r') as cfg:
-    remotes.update(json.load(cfg))
+    nodes = json.load(cfg)
 
 with open('customers.json', 'r') as cfg:
-    remotes.update(json.load(cfg))
+    customers = json.load(cfg)
 
-with open('sshconfig', 'w') as out:
-    for name, data in remotes.items():
-        user = data[USER_KEY] if USER_KEY in data else DEFAULT_USER
-        out.write(f"Host sbas-{name}\n")
-        out.write(f"    HostName {data[HOST_KEY]}\n")
-        out.write(f"    User {user}\n\n")
+def gen_sshconfig():
+    DEFAULT_USER = 'scionlab'
 
+    with open(os.path.join(GEN_DIR, 'sshconfig'), 'w') as f:
+        remotes = {}
+        remotes.update(nodes)
+        remotes.update(customers)
+        for name, data in remotes.items():
+            user = data[USER_KEY] if USER_KEY in data else DEFAULT_USER
+            f.write(f"Host sbas-{name}\n")
+            f.write(f"    HostName {data[HOST_KEY]}\n")
+            f.write(f"    User {user}\n\n")
+
+def gen_client_cfgs():
+    for name, data in customers.items():
+        cfg = data.copy()
+        del cfg[HOST_KEY]
+        del cfg[USER_KEY]
+        with open(os.path.join(GEN_DIR, f"client-{name}.json"), 'w') as f:
+            json.dump(cfg, f)
+
+if __name__ == '__main__':
+    gen_sshconfig()
+    gen_client_cfgs()
